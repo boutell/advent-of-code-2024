00811c6191dd53b53a9a816e59955280b05228ee
Tom Boutell <tom@apostrophecms.com>
2025-01-04 14:09:57 -0500

    5a

---
+++ 5a.mjs
@@ -0,0 +1,40 @@
+import readLines from './lib/lines.mjs';
+import log from './lib/log.mjs';
+
+const lines = readLines();
+
+const rules = [];
+const updates = [];
+
+for (const line of lines) {
+  if (line.includes('|')) {
+    rules.push(line.split('|').map(s => parseInt(s)));
+  } else if (line.includes(',')) {
+    updates.push(line.split(',').map(s => parseInt(s)));
+  } else {
+    throw new Error(`Unexpected line: ${line}`);
+  }
+}
+
+let sum = 0;
+for (const update of updates) {
+  let good = true;
+  for (const [before, after] of rules) {
+    const beforeIndex = update.indexOf(before);
+    if (beforeIndex === -1) {
+      continue;
+    }
+    const afterIndex = update.indexOf(after);
+    if (afterIndex === -1) {
+      continue;
+    }
+    if (beforeIndex > afterIndex) {
+      good = false;
+      break;
+    }
+  }
+  if (good) {
+    sum += update[Math.floor(update.length / 2)];
+  }
+}
+log(sum);
