0b7c2489bd99f5f2553bf64e520ffc69651e55e3
Tom Boutell <tom@apostrophecms.com>
2025-01-03 20:06:37 -0500

    day 2

---
+++ 2b.mjs
@@ -0,0 +1,41 @@
+import readLines from './lib/lines.mjs';
+import log from './lib/log.mjs';
+
+const reports = readLines({ split: true, integers: true });
+
+let safe = 0;
+for (const report of reports) {
+  const variations = [ report ];
+  for (let i = 0; (i < report.length); i++) {
+    variations.push([...report.slice(0, i), ...report.slice(i + 1)]);
+  }
+  let ok = false;
+  for (const variation of variations) {
+    if (isSafe(variation)) {
+      ok = true;
+      break;
+    }
+  }
+  if (ok) {
+    safe++;
+  }
+}
+log(safe);
+
+function isSafe(report) {
+  if (report.length < 2) {
+    return true;
+  }
+  const sign = Math.sign(report[1] - report[0]);
+  for (let i = 0; (i < report.length - 1); i++) {
+    const diff = report[i + 1] - report[i];
+    if (Math.sign(diff) !== sign) {
+      return false;
+    }
+    const dist = Math.abs(diff);
+    if ((dist < 1) || (dist > 3)) {
+      return false;
+    }
+  }
+  return true;  
+}
