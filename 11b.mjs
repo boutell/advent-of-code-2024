b7d63c84ae24206353803b2b7b2415ccadb7aa1b
Tom Boutell <tom@apostrophecms.com>
2025-01-05 16:01:18 -0500

    memoized recursive solution

--- 11b.mjs
+++ 11b.mjs
@@ -1,6 +1,6 @@
 import readLines from './lib/lines.mjs';
 import log from './lib/log.mjs';
-import { swap } from './lib/array.mjs';
+import memoize from './lib/memoize.mjs';
 
 const lines = readLines({ split: true, integers: true });
 
@@ -16,34 +16,30 @@ const seen = new Set();
 // so if I've seen what "1" does for 10 generations,
 // I can substitute that, or even the count for that.
 
+const simulate = memoize(simulateBody);
+
 let sum = 0;
 for (const val of input) {
-  sum += simulate(val, 25);
+  sum += simulate(val, 75);
 }
 log(sum);
 
-function simulate(n, generations) {
-  let input = [ n ];
-  for (let i = 0; (i < generations); i++) {
-    console.log('pass ' + i + ' of ' + generations);
-    const next = [];
-    for (const val of input) {
-      if (val === 0) {
-        next.push(1);
-        continue;
-      }
-      const s = val.toString();
-      if (!(s.length % 2)) {
-        const half = s.length / 2;
-        const left = parseInt(s.substring(0, half));
-        const right = parseInt(s.substring(half, s.length));
-        next.push(left, right);
-        continue;
-      }
-      next.push(val * 2024);
-    }
-    input = next;
-    console.log(`${n} ${input.length}`);
+function simulateBody(val, generations) {
+  if (val === 0) {
+    return next(1);
   }
-  return input.length;
+  const s = val.toString();
+  if (!(s.length % 2)) {
+    const half = s.length / 2;
+    const left = parseInt(s.substring(0, half));
+    const right = parseInt(s.substring(half, s.length));
+    return next(left) + next(right);
+  }
+  return next(val * 2024);
+  function next(val) {
+    if (generations === 1) {
+      return 1;
+    }
+    return simulate(val, generations - 1);
+  }
 }
